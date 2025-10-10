db40e19ad18248003241bf82ac5cb98c2e6c033c
JOjo <jolinlin.tan@hotmail.com>
2025-10-10 20:38:33 +0800

    update 2

--- Assets/Player/GameInfo.cs
+++ Assets/Player/GameInfo.cs
@@ -1,3 +1,28 @@
+//using System;
+//using UnityEngine;
+
+//[CreateAssetMenu(fileName = "GameInfo", menuName = "Scriptable Objects/GameInfo")]
+//public class GameInfo : ScriptableObject
+//{
+//    [Header("Base Values")]
+//    [SerializeField] private float maxTimer;
+//    //public bool _runTimer;
+//    //[SerializeField] private float someinventorychecklistshit;
+
+//    [Header("Changing Values")]
+//    public float currentTimer;
+
+//    public void ResetValues()
+//    {
+//        currentTimer = maxTimer;
+//    }
+
+//    internal bool UpdateValues(object dt)
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+
 using System;
 using UnityEngine;
 
@@ -11,6 +36,14 @@ public class GameInfo : ScriptableObject
 
     [Header("Changing Values")]
     public float currentTimer;
+
+    //public void UpdateValues(float dt)
+    //{
+    //    if (_runTimer)
+    //        currentTimer -= dt;
+    //    else
+    //        ResetValues();
+    //}
 
     public void ResetValues()
     {
--- Assets/Player/GameManager.cs
+++ Assets/Player/GameManager.cs
@@ -1,5 +1,92 @@
+//using NUnit.Framework.Interfaces;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.Events;
+
+//public class GameManager : MonoBehaviour
+//{
+//    [SerializeField] private GameInfo gameInfo;
+//    [SerializeField] private PlayerController player;
+//    [SerializeField] private UnityEvent startNewGame;
+//    [SerializeField] private TMP_Text timer;
+//    [SerializeField] private Vector3 spawnPosition;
+//    [SerializeField] private AudioSource _backgroundMusic;
+
+//    public bool updateTimer;
+//    public bool endGame;
+//    public EndGameScript endGameScript;
+
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        // Temp
+//        startNewGame.Invoke();
+//    }
+
+//    private void Update()
+//    {
+//        if (gameInfo.currentTimer <= 0)
+//        {
+//            endGame = true;
+//            gameInfo.ResetValues();
+//            gameEnded();
+//        }
+//        else
+//        {
+//            if (updateTimer)
+//            {
+//                gameInfo.currentTimer -= Time.deltaTime;
+//                //timer.text = Mathf.Floor(gameInfo.currentTimer / 60) + ":" + (Mathf.Floor(gameInfo.currentTimer % 60 * 10) / 10).ToString();
+//                timer.text = Mathf.Floor(gameInfo.currentTimer / 60) + ":" + Mathf.Floor(gameInfo.currentTimer % 60).ToString();
+//            }
+//        }
+//    }
+
+//    public void ToggleTimer(bool temp)
+//    {
+//        updateTimer = temp;
+//    }
+
+//    public void PauseGame()
+//    {
+//        Time.timeScale = 0;
+//    }
+
+//    public void UnpauseGame()
+//    {
+//        Time.timeScale = 1;
+//    }
+
+//    public void RestartGame()
+//    {
+//        updateTimer = false;
+//        startNewGame.Invoke();
+//        gameInfo.ResetValues();
+//        endGame = false;
+//        player.transform.position = spawnPosition;
+//    }
+
+//    private void gameEnded()
+//    {
+//        endGameScript.countNPCS();
+//        endGameScript.countFood();
+//        endGameScript.initDialogue();
+//        endGameScript.gameObject.SetActive(true);
+//        endGame = false;
+//    }
+
+//    public void playBGM()
+//    {
+//        _backgroundMusic.Play();
+//    }
+
+//    public void stopBGM()
+//    {
+//        _backgroundMusic.Stop();
+//    }
+//}
+
 using NUnit.Framework.Interfaces;
-using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,11 +95,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameInfo gameInfo;
     [SerializeField] private PlayerController player;
     [SerializeField] private UnityEvent startNewGame;
-    [SerializeField] private TMP_Text timer;
-    [SerializeField] private Vector3 spawnPosition;
-    [SerializeField] private AudioSource _backgroundMusic;
-
-    public bool updateTimer;
     public bool endGame;
     public EndGameScript endGameScript;
 
@@ -22,7 +104,6 @@ public class GameManager : MonoBehaviour
         // Temp
         startNewGame.Invoke();
     }
-
     private void Update()
     {
         if (gameInfo.currentTimer <= 0)
@@ -33,37 +114,15 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            if (updateTimer)
-            {
-                gameInfo.currentTimer -= Time.deltaTime;
-                //timer.text = Mathf.Floor(gameInfo.currentTimer / 60) + ":" + (Mathf.Floor(gameInfo.currentTimer % 60 * 10) / 10).ToString();
-                timer.text = Mathf.Floor(gameInfo.currentTimer / 60) + ":" + Mathf.Floor(gameInfo.currentTimer % 60).ToString();
-            }
+            gameInfo.currentTimer -= Time.deltaTime;
         }
     }
 
-    public void ToggleTimer(bool temp)
+    public void ResetValues()
     {
-        updateTimer = temp;
-    }
-
-    public void PauseGame()
-    {
-        Time.timeScale = 0;
-    }
-
-    public void UnpauseGame()
-    {
-        Time.timeScale = 1;
-    }
-
-    public void RestartGame()
-    {
-        updateTimer = false;
         startNewGame.Invoke();
         gameInfo.ResetValues();
         endGame = false;
-        player.transform.position = spawnPosition;
     }
 
     private void gameEnded()
@@ -73,15 +132,5 @@ public class GameManager : MonoBehaviour
         endGameScript.initDialogue();
         endGameScript.gameObject.SetActive(true);
         endGame = false;
-    }
-
-    public void playBGM()
-    {
-        _backgroundMusic.Play();
-    }
-
-    public void stopBGM()
-    {
-        _backgroundMusic.Stop();
     }
 }
